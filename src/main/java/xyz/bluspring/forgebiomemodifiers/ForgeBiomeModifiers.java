934793c1e68d6581521d26df401cc24107b21b4e
Naz <bluspringindohq@gmail.com>
2024-01-21 18:30:42 +0800

    Use Porting Lib's built-in dynamic registries

--- src/main/java/xyz/bluspring/forgebiomemodifiers/ForgeBiomeModifiers.java
+++ src/main/java/xyz/bluspring/forgebiomemodifiers/ForgeBiomeModifiers.java
@@ -1,8 +1,8 @@
 package xyz.bluspring.forgebiomemodifiers;
 
+import io.github.fabricators_of_create.porting_lib.registries.RegistryEvents;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.resources.RegistryDataLoader;
-import xyz.bluspring.forgebiomemodifiers.events.RegisterDatapackRegistryCallback;
 import xyz.bluspring.forgebiomemodifiers.structures.StructureModifier;
 import xyz.bluspring.forgebiomemodifiers.structures.StructureModifiers;
 import xyz.bluspring.forgebiomemodifiers.worldgen.BiomeModifier;
@@ -11,9 +11,9 @@ import xyz.bluspring.forgebiomemodifiers.worldgen.BiomeModifiers;
 public class ForgeBiomeModifiers implements ModInitializer {
     @Override
     public void onInitialize() {
-        RegisterDatapackRegistryCallback.EVENT.register((lists) -> {
-            lists.add(new RegistryDataLoader.RegistryData<>(BiomeModifiers.BIOME_MODIFIER_KEY, BiomeModifier.DIRECT_CODEC));
-            lists.add(new RegistryDataLoader.RegistryData<>(StructureModifiers.STRUCTURE_MODIFIER_KEY, StructureModifier.DIRECT_CODEC));
+        RegistryEvents.NEW_DATAPACK_REGISTRY.register((registry) -> {
+            registry.register(new RegistryDataLoader.RegistryData<>(BiomeModifiers.BIOME_MODIFIER_KEY, BiomeModifier.DIRECT_CODEC));
+            registry.register(new RegistryDataLoader.RegistryData<>(StructureModifiers.STRUCTURE_MODIFIER_KEY, StructureModifier.DIRECT_CODEC));
         });
 
         StructureModifiers.STRUCTURE_MODIFIER_SERIALIZERS.register();
