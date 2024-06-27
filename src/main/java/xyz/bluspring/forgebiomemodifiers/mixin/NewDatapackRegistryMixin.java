6ddf32b3e3f75c11f71be3529bcab654a62c718a
Naz <bluspringindohq@gmail.com>
2024-06-28 01:01:20 +0800

    okay, i guess *do* remap then

--- src/main/java/xyz/bluspring/forgebiomemodifiers/mixin/NewDatapackRegistryMixin.java
+++ src/main/java/xyz/bluspring/forgebiomemodifiers/mixin/NewDatapackRegistryMixin.java
@@ -9,7 +9,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(value = RegistryEvents.NewDatapackRegistry.class, remap = false)
+@Mixin(value = RegistryEvents.NewDatapackRegistry.class)
 public class NewDatapackRegistryMixin {
     @Inject(method = "register(Lnet/minecraft/resources/RegistryDataLoader$RegistryData;Lcom/mojang/serialization/Codec;)V", at = @At("TAIL"))
     private <T> void addDynamicRegistry(RegistryDataLoader.RegistryData<T> registryData, @Nullable Codec<T> networkCodec, CallbackInfo ci) {
